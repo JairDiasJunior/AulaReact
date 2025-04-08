e6bbdb918d494036b814a20bfc908bb8ca5ce330
Jair Junior <carlosdiasjuniorjair@gmail.com>
2025-04-08 12:22:48 -0300

    08/04

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,21 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+  
+  return (
+   <div> 
+    <h1>Boas Vindas ao com JavaScript</h1>
+
+   <p>
+    Este é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React utilizando JavaScript
+   </p>
+
+   <BoasVindas nome="Jair"/>
+   <BoasVindas nome="Augusto"/>
+   <BoasVindas nome="Hélio"/>
+    </div>
+
+  )
+}
+
+export default App
